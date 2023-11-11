cb1f783a94bef985bcba270b5ab7606fc66660e6
JONATAS PEREIRA DO NASCIMENTO <202109069934@alunos.estacio.br>
2023-11-11 12:25:16 -0300

    Tela Novo Treinamentos Lista

--- app/src/main/java/com/example/smartmanager/TelaTreinamentos.java
+++ app/src/main/java/com/example/smartmanager/TelaTreinamentos.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class TelaTreinamentos extends AppCompatActivity {
     Button novotreinamento;
+
+    ListView listatreinamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,13 @@ public class TelaTreinamentos extends AppCompatActivity {
                 startActivity(new Intent(TelaTreinamentos.this, TelaNovoTreinamento.class));
             }
         });
+
+        listatreinamentos = (ListView) findViewById(R.id.ListViewListaTreinamentos);
+
+        String [] treinados = new String[]{"\nC++\nAtraso\n04/11/2020\n","\nCiclano de tal\nAtraso na reunião\n04/11/2020\n","\nAstrobaldo de tal\nBaixo rendimento\n04/11/2020\n"};
+
+        ArrayAdapter <String> novotreinamentoadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,treinados);
+
+        listatreinamentos.setAdapter(novotreinamentoadapter);
     }
 }
