44b6014a54a3a6f0d03e163c88c4471b1b27ed30
JONATAS PEREIRA DO NASCIMENTO <202109069934@alunos.estacio.br>
2023-11-11 10:56:47 -0300

    Primeiro commit tela treinamentos

---
+++ app/src/main/java/com/example/smartmanager/TelaTreinamentos.java
@@ -0,0 +1,14 @@
+package com.example.smartmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaTreinamentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_treinamentos);
+    }
+}
