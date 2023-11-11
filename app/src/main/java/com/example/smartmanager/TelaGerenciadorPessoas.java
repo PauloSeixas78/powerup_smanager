e57e76470e195f6a654e1f7b7483a0eee00ea566
JONATAS PEREIRA DO NASCIMENTO <202109069934@alunos.estacio.br>
2023-11-11 11:22:52 -0300

    Tela Treinamentos

--- app/src/main/java/com/example/smartmanager/TelaGerenciadorPessoas.java
+++ app/src/main/java/com/example/smartmanager/TelaGerenciadorPessoas.java
@@ -2,13 +2,24 @@ package com.example.smartmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaGerenciadorPessoas extends AppCompatActivity {
-
+    Button treinamentos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_gerenciador_pessoas);
+        treinamentos = findViewById(R.id.buttonTreinamentos);
+
+        treinamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TelaGerenciadorPessoas.this, TelaTreinamentos.class));
+            }
+        });
     }
 }
