94b544e05abc0216f1b9cf27de2e1e0c19160116
PAULO ROBERTO BELEM DE SEIXAS <61366935268@professores.estacio.br>
2023-11-11 10:26:12 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/smartmanager/TelaGerenciadorPessoas.java
@@ -0,0 +1,14 @@
+package com.example.smartmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaGerenciadorPessoas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_gerenciador_pessoas);
+    }
+}
