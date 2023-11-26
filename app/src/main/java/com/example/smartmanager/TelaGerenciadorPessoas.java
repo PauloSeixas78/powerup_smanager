3850873458ad5a96c43a50d92683362975a66367
PAULO ROBERTO BELEM DE SEIXAS <61366935268@professores.estacio.br>
2023-11-26 10:44:28 -0300

    Tela lista feedbacks

--- app/src/main/java/com/example/smartmanager/TelaGerenciadorPessoas.java
+++ app/src/main/java/com/example/smartmanager/TelaGerenciadorPessoas.java
@@ -8,12 +8,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class TelaGerenciadorPessoas extends AppCompatActivity {
+    Button feedbacks;
     Button treinamentos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_gerenciador_pessoas);
         treinamentos = findViewById(R.id.buttonTreinamentos);
+        feedbacks = findViewById(R.id.buttonTelaGerenciamentoPessoasFeedbacks);
+
+        feedbacks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TelaGerenciadorPessoas.this, TelaListaFeedbacks.class));
+            }
+        });
 
         treinamentos.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/smartmanager/TelaListaFeedbacks.java
@@ -0,0 +1,14 @@
+package com.example.smartmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaListaFeedbacks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_lista_feedbacks);
+    }
+}
