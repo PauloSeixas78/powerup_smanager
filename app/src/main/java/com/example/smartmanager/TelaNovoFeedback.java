1c20188ad1b01433318b669ca922b8d70459d3d4
BRUNO GUSTAVO CARDOSO DUARTE <202103419976@alunos.estacio.br>
2023-11-27 20:56:06 -0300

    Meu 7

--- app/src/main/java/com/example/smartmanager/TelaNovoFeedback.java
+++ app/src/main/java/com/example/smartmanager/TelaNovoFeedback.java
@@ -1,13 +1,21 @@
 package com.example.smartmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class TelaNovoFeedback extends AppCompatActivity {
 
-    EditText Matricula;
-    EditText Situação;
+    Databasehelper helper;
+
+    Spinner Matricula;
+    EditText Situacao;
     EditText Comportamento;
     EditText Impacto;
 
@@ -17,16 +25,28 @@ public class TelaNovoFeedback extends AppCompatActivity {
         setContentView(R.layout.activity_tela_novo_feedbacks);
 
         Matricula = findViewById(R.id.spinnerMatricula);
-        Situação = findViewById(R.id.editTextTextSituacao);
+        Situacao = findViewById(R.id.editTextTextSituacao);
         Comportamento = findViewById(R.id.editTextTextComportamento);
         Impacto = findViewById(R.id.editTextTextImpacto);
+        helper = new Databasehelper(this);
+
+    }
+
+    public void salvar(View view){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+        valores.put("Matricula",Matricula.getSelectedItem().toString());
+        valores.put("Situacao",Situacao.getText().toString());
+        valores.put("Comportamento",Comportamento.getText().toString());
+        valores.put("Impacto",Impacto.getText().toString());
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        Matricula.setText("");
-        Situação.setText("");
+        //Matricula.("");
+        Situacao.setText("");
         Comportamento.setText("");
         Impacto.setText("");
     }
